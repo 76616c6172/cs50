9c2934334f791adebb0da30050ff7dd936737010
valar <>
2021-04-15 11:15:31 -0700

    FIX: Memory Bug. compute_score() now takes into account the correct length of the word when creating array and iterating through chars.

--- week2/scrabble.c
+++ week2/scrabble.c
@@ -21,36 +21,37 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 
 //int compute_score(char word[]);
 //Computes and returns score for string
-int compute_score(char word[]) {
+int compute_score(string word) {
 	char wordUpper[strlen(word)];
 
 	//convert string to uppercase
 	for (int i = 0; i < strlen(word); i++) {
 		wordUpper[i] = toupper(word[i]);
 	}
+	
 	//Iterate over the chars of the string and calculate its score
 	int score = 0;
-	int letter;
+	int letterPosition;
+	for (int a = 0; a < strlen(word); a++) {
+		letterPosition = ((int)(wordUpper[a])) -65;
 
-	for (int a = 0; a < strlen(wordUpper); a++) {
-		letter = (int)(wordUpper[a])-65;
-		//debug printf("character: %c\nletter: %d\n", wordUpper[a], letter);
-		score += POINTS[letter];
+		//debug 	
+		printf("%c(%d)+ ", wordUpper[a], POINTS[letterPosition]);
+		//printf("character: %c\nletter: %d\n", wordUpper[a], letterPosition);
+		score += POINTS[letterPosition];
 	}
+		printf("= %d\n", score);
 	return score;
 }
 
 int main(void) {
     //Get input words from both players
-    char word1[1000]; strcpy(word1, get_string("Player 1: "));
-    char word2[1000]; strcpy(word2, get_string("Player 2: "));
+		string word1 = get_string("Player 1: ");
+		string word2 = get_string("Player 2: ");
 
     //Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
-
-		//Print the scores for debugging
-		//printf("%s: scored %d\n%s: scored %d\n",word1 ,score1, word2, score2);
 
     //Print the winner
 		if ( score1 > score2) {
@@ -60,4 +61,5 @@ int main(void) {
 		}else{
 			printf("Tie!\n");
 		}
+	return 0;
 }
